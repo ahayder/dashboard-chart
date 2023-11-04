7fa3f90b97b27363c6530b3f787752b24da647ea
Ali Hayder <alihayder19@gmail.com>
2023-11-04 19:29:33 +0600

    initial state separeted

--- redux/dashboardSlice.js
+++ redux/dashboardSlice.js
@@ -1,72 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import initialState from "./initialState";
 
 export const dashboardSlice = createSlice({
   name: "dashboard",
-  initialState: {
-    barChart: {
-      chart: {
-        type: "bar",
-      },
-      title: {
-        text: "Sample Bar Chart",
-      },
-      series: [
-        {
-          data: [1, 2, 3, 4, 5],
-        },
-      ],
-    },
-    boxWhiskerPlot: {
-      chart: {
-        type: "boxplot",
-      },
-      title: {
-        text: "Sample Box and Whisker Plot",
-      },
-      series: [
-        {
-          data: [
-            [760, 801, 848, 895, 965],
-            [733, 853, 939, 980, 1080],
-          ],
-        },
-      ],
-    },
-    scatterChart: {
-      chart: {
-        type: "scatter",
-      },
-      title: {
-        text: "Sample Scatter Chart",
-      },
-      series: [
-        {
-          data: [
-            [1, 2],
-            [2, 4],
-            [3, 6],
-          ],
-        },
-      ],
-    },
-    areaRangeChart: {
-      chart: {
-        type: "arearange",
-      },
-      title: {
-        text: "Sample Area Range Chart",
-      },
-      series: [
-        {
-          data: [
-            [1, 2, 3],
-            [2, 3, 4],
-            [3, 4, 5],
-          ],
-        },
-      ],
-    },
-  },
+  initialState: initialState,
   reducers: {
     changeChartType: (state, action) => {
       const { key, type } = action.payload;
